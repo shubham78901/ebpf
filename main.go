7f095a60e9876faeb4866ce9766680053123b1ff
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-02-02 19:25:43 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+func main() {
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal("Removing memlock:", err)
+	}
+
+	// Load the compiled eBPF ELF and load it into the kernel.
+	var objs xdpObjects
+	if err := loadXdpObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects:", err)
+	}
+	defer objs.Close()
+
+	ifname := "eth0" // Change this to an interface on your machine.
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		log.Fatalf("Getting interface %s: %s", ifname, err)
+	}
+
+	// Attach count_packets to the network interface.
+	link, err := link.AttachXDP(link.XDPOptions{
+		Program:   nil,
+		Interface: iface.Index,
+	})
+	if err != nil {
+		log.Fatal("Attaching XDP:", err)
+	}
+	defer link.Close()
+
+	log.Printf("Counting incoming packets on %s..", ifname)
+
+	// Periodically fetch the packet counter from PktCount,
+	// exit the program when interrupted.
+	tick := time.Tick(time.Second)
+	stop := make(chan os.Signal, 5)
+	signal.Notify(stop, os.Interrupt)
+	for {
+		select {
+		case <-tick:
+
+			log.Printf("Received %d packets", 1)
+		case <-stop:
+			log.Print("Received signal, exiting..")
+			return
+		}
+	}
+}
---
+++ xdp.c
@@ -0,0 +1,35 @@
+/*
+ * Do not allow TCP port 8080 to connect by dropping corr pkts.
+ *
+ * Check: block-tcp-8080.rst for details
+ */
+
+#include <arpa/inet.h> 
+
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+// Sizeof all headers till TCP
+#define TOTSZ (sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr))
+
+int xdp_drop(struct xdp_md *ctx)
+{
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    struct iphdr *ip = data + sizeof(struct ethhdr);
+    struct tcphdr *tcph = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+
+    if (data + TOTSZ > data_end) {
+        return XDP_PASS;
+    }
+
+    if (ip->protocol == IPPROTO_TCP && tcph->source == htons(8080)) {
+        return XDP_DROP;
+    }
+
+    return XDP_PASS;
+}
